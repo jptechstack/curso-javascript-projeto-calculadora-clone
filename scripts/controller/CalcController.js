d30c0090d3ce17a84508e3a3ef03e5e0de29d473
jptechstack <jptechstack@email.com>
2026-07-13 13:09:36 -0300

    Manipulando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,23 +2,34 @@ class CalcController {
 
     constructor() {
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = this._displayCalc;
+        dateEl.innerHTML = '13/07/2026';
+        timeEl.innerHTML = '13:08';
     }
 
     get displayCalc() {
         return this._displayCalc;
     }
 
-    set displayCalc(valor) {
-        this._displayCalc = valor;
+    set displayCalc(value) {
+        this._displayCalc = value;
     }
 
     get dataAtual() {
-        return this._dataAtual;
+        return this._currentDate;
     }
 
-    set dataAtual(valor) {
-        this._dataAtual = valor;
+    set dataAtual(value) {
+        this._currentDate = value;
     }
 
 }
