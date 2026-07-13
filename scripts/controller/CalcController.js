dfa59fd468441bbdb8c8d826030fa0d6913f0605
jptechstack <jptechstack@email.com>
2026-07-13 14:11:59 -0300

    Data e Hora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,34 +1,60 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = "0";
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
     }
 
     initialize() {
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
 
-        displayCalcEl.innerHTML = this._displayCalc;
-        dateEl.innerHTML = '13/07/2026';
-        timeEl.innerHTML = '13:08';
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+    
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value) {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual() {
-        return this._currentDate;
+    get currentDate() {
+        return new Date();
     }
 
-    set dataAtual(value) {
+    set currentDate(value) {
         this._currentDate = value;
     }
 
