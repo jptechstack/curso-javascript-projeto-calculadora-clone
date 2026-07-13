285325432a4a5e393cb90a09d355e23266aaaaf9
jptechstack <jptechstack@email.com>
2026-07-13 14:45:51 -0300

    Aplicando vários Eventos usando split()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -18,25 +18,43 @@ class CalcController {
 
     }
 
+    addEventListenerAll(element, events, fn) {
+       
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        });
+    }
+
     initButtonsEvents() {
+
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
 
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
 
                 console.log(btn.className.baseVal.replace("btn-", ""));
+
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
 
             });
         })
     }
 
     setDisplayDateTime() {
+
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
             year: "numeric"
         });
+
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
