73bda9c9efe01207a9072a05e0cbbb20d6a08a11
jptechstack <jptechstack@email.com>
2026-07-14 10:16:17 -0300

    Correção de ugs com o Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -195,7 +195,7 @@ class CalcController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -212,6 +212,8 @@ class CalcController {
     addDot() {
 
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if(this.isOperator(lastOperation) || !lastOperation) {
 
